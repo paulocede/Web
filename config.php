566214ce5ae87959d5ce80e0531e90ff759be1de
paulocede <pablo_cede92@hotmail.com>
2022-01-26 23:34:54 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
+/* Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
